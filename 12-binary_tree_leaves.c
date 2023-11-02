83561723589858439ac40fb837ba81f0df9d3615
rhf007 <rahofahazem512@gmail.com>
2023-11-02 07:25:33 -0700

    turns out a number makes all the difference

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,4 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * 
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree:  pointer to the root node of the tree to count the number of leaves
+ * Return: count, If tree is NULL, the function must return 0
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leafnum = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+
+	leafnum += binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+
+	return (leafnum);
+}
