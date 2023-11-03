680a1f17f86beed20fa50faeb8307e94b81c0bb2
rhf007 <rahofahazem512@gmail.com>
2023-11-03 12:44:33 -0700

    uncle of sus ez af

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -29,15 +29,15 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-        if (node == NULL || node->parent == NULL)
-        {
-                return (NULL);
-        }
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
 
-        if (node->parent->left == node)
-        {
-                return (node->parent->right);
-        }
+	if (node->parent->left == node)
+	{
+		return (node->parent->right);
+	}
 
-        return (node->parent->left);
+	return (node->parent->left);
 }
